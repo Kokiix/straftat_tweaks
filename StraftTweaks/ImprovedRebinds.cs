17f4665f762d7e3a81cbd4be36820ddfa200d931
kenneth <kokiidoki@gmail.com>
2026-05-27 11:01:23 -0400

    see change in menu but actual bind hasnt changed

--- StraftTweaks/ImprovedRebinds.cs
+++ StraftTweaks/ImprovedRebinds.cs
@@ -3,24 +3,45 @@ using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using HarmonyLib;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ImprovedRebinds;
 
-[HarmonyPatch(typeof(InputManager), "Awake")]
-static class Test
+/// <summary>
+/// Allow for key binds to swap, and scroll bind to jump
+/// </summary>
+[HarmonyPatch(typeof(InputManager), "DoRebind")]
+static class ScrollBindFixer
 {
-    static void Postfix()
+    static void Prefix(InputAction actionToRebind, ref int bindingIndex, TextMeshProUGUI statusText, ref bool allCompositeParts, bool excludeMouse, bool sequenceDisplay)
     {
-        Debug.LogError("test");
+        Debug.LogError(actionToRebind);
+        Debug.LogError(bindingIndex);
+
+        Debug.LogError(actionToRebind.name);
+        if (actionToRebind.name == "ChangeWeapon")
+        {
+            actionToRebind.Disable();
+            actionToRebind.expectedControlType = "Button";
+
+            // remove mouse bind
+            // actionToRebind.bindings.Do(b => Debug.LogError(b.path));
+            int scrollIndex = actionToRebind.bindings.IndexOf(b => b.path == "<Mouse>/scroll");
+            if (scrollIndex != -1)
+            {
+                actionToRebind.ChangeBinding(scrollIndex).Erase();
+            }
+
+            // find/create composite bind
+            var newBindIdx = actionToRebind.bindings.IndexOf(b => b.isPartOfComposite);
+            if (newBindIdx == -1)
+                newBindIdx = actionToRebind.AddCompositeBinding("2DVector")
+                .With("Up", "<Keyboard>/T")
+                .bindingIndex + 1;
+
+            bindingIndex = newBindIdx;
+        }
     }
 }
-
-// static class ModMenu
-// {
-//     [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-//     internal static void ConfigBuilder(OptionListContext c)
-//     {
-//         // c.AppendButton("Apply Changes", "Apply Changes", SetOutlineColors.UpdateColorsFromConfig);
-//     }
-// }
